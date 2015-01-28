f794fa957b663979390cb5cd560aaadae19d54b1
root <jdunderwood@gmail.com>
2015-01-28 13:36:03 -0700

    changes to views and templates for customers and vehicles

--- wrenchmarks/wrenchmarks/urls.py
+++ wrenchmarks/wrenchmarks/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from wrenchmarks_app.views import CustomerList
+from wrenchmarks_app.views import CustomerList, VehicleList, VehicleDetail
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'wrenchmarks.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', CustomerList.as_view()), 
+    url(r'^$', CustomerList.as_view()),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^cust/(?P<customer_id>\d+)$', VehicleList.as_view()),
+    url(r'^vehicle/(?P<pk>\d+)$', VehicleDetail.as_view()),
 )
--- wrenchmarks/wrenchmarks_app/views.py
+++ wrenchmarks/wrenchmarks_app/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from wrenchmarks_app.forms import CustomerForm
 from django.views.generic.edit import FormView
-from django.views.generic import ListView
-from wrenchmarks_app.models import Customer
+from django.views.generic import ListView, DetailView
+from wrenchmarks_app.models import Customer, Vehicle
 
 # Create your views here.
 
 class CustomerList(ListView):
-	template_name = 'customer.html'
+	template_name = 'customer_list.html'
 	model = Customer
+
+class VehicleList(ListView):
+	template_name = 'customer_vehicles.html'
+
+	def get_queryset(self):
+		return Vehicle.objects.filter(customer_id=self.kwargs.get("customer_id", None))
+
+class VehicleDetail(DetailView):
+	template_name = 'vehicle_detail.html'
+	model = Vehicle
