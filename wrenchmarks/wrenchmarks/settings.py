df256841d687bd8c1fc7b7b8823f9f5944dc28ca
root <jdunderwood@gmail.com>
2015-02-06 16:39:55 -0700

    updated to use bootstrap

--- wrenchmarks/wrenchmarks/settings.py
+++ wrenchmarks/wrenchmarks/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'bootstrap3',
+    'django_admin_bootstrapped.bootstrap3',
+    'django_admin_bootstrapped',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
